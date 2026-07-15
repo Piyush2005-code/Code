5fc78c0cfa3fbdb16fb3a79fced3bd6c66aa5f21
Piyush2005-code <piyush.bhati680@gmail.com>
2026-07-15 23:30:48 +0530

    Updates: some changes in the files but are useless for now

---
+++ simple_ques/maxSlidingWindow.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+
+
+using namespace std;
+
+
+class Solution{
+    public:
+        vector<int> solve(){
+            
+
+
+        }
+};
+
+
+int main(){
+
+
+
+}
+
